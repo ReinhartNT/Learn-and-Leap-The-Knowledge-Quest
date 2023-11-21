27f2a09c0652c0b43bbf787ac3f3d53b893f0039
Reinhart <cloudofpigs@gmail.com>
2023-11-21 16:51:00 +0700

    Over

--- src/GameOverScene.js
+++ src/GameOverScene.js
@@ -6,15 +6,36 @@ export default class GameOverScene extends Phaser.Scene{
     }
 
     init(data){
+        this.RestartButton = undefined
 
+        // Score
+        this.score = data.score
     }
 
     preload(){
     this.load.image('BG', 'Images/BG/PNG/summer8/Summer8.png')
+    this.load.image('RestartButton', 'Images/PNG/yellow_button00.png')
+    this.load.image('Sad', 'Images/9479419-removebg-preview.png')
+    this.load.image('Logo', 'Images/learn-and-leap-the-knowledge-quest-high-resolution-logo-transparent.png')
     }
 
     create(){
     this.add.image(250, 75, 'BG')
+    this.RestartButton = this.add.image(520, 350, 'RestartButton').setInteractive().setScale(1.5)
+    this.add.text(460, 330, 'Restart', {fontSize: '32px', color: '#fff', fontFamily: 'Arial', fontStyle: 'bold'})
+    this.RestartButton.once('pointerup', () => {
+        this.scene.start('world-scene')
+    })
+
+    this.add.text(390, 200, "Score: " + this.score, {
+        fontSize: "32px",
+        color: "#fff",
+        fontFamily: "Arial",
+        fontStyle: "bold",
+    });
+
+    this.add.image(430, 40, 'Sad').setScale(0.75)
+    this.add.image(100, 50, 'Logo').setScale(0.1)
     }
 
     update(time){
